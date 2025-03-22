f3013b4ba87b698fb859134a538e46bf7f53de9e
roberth <be6323787@gmail.com>
2025-03-21 19:10:43 -0500

    subo inicio del proyecto

---
+++ src/main/java/com/rober/bak/entity/User.java
@@ -0,0 +1,43 @@
+package com.rober.bak.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate modifiedAt;
+
+}
---
+++ src/test/java/com/rober/bak/BakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rober.bak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
